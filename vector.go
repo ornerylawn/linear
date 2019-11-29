f8bd7fddc8f6c1f4cc769759dc99a5a6dbb3699a
Ryan Brown <ryan@ryanleebrown.com>
2019-11-28 22:53:03 -0800

    Implement vector with a slice directly

--- vector.go
+++ vector.go
@@ -6,18 +6,12 @@ type Vector interface {
 	Set(i int, value float64)
 }
 
-type ArrayVector struct {
-	array []float64
-	dims  int
-}
+type ArrayVector []float64
 
 func NewArrayVector(dims int) Vector {
-	return &ArrayVector{
-		array: make([]float64, dims),
-		dims:  dims,
-	}
+	return ArrayVector(make([]float64, dims))
 }
 
-func (v *ArrayVector) Dimensions() int          { return v.dims }
-func (v *ArrayVector) Get(i int) float64        { return v.array[i] }
-func (v *ArrayVector) Set(i int, value float64) { v.array[i] = value }
+func (v ArrayVector) Dimensions() int          { return len(v) }
+func (v ArrayVector) Get(i int) float64        { return v[i] }
+func (v ArrayVector) Set(i int, value float64) { v[i] = value }
